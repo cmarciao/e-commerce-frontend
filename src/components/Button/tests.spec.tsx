109619371065d4b97be64c8828d25b0359b843a7
Cássio Marcião <marciaocassio@gmail.com>
2023-09-17 22:24:52 -0300

    tests: Add units test to the button component

---
+++ src/components/Button/tests.spec.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { Button } from '.';
+
+describe('Button component', () => {
+    it('should render correctly', () => {
+        render(<Button>Test</Button>);
+
+        expect(screen.getByText('Test')).toBeInTheDocument();
+    });
+
+    it('should render loading in button', () => {
+        render(<Button isLoading>Test</Button>);
+
+        expect(screen.queryByText('Test')).not.toBeInTheDocument();
+    });
+});
