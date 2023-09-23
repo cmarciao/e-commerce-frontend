d03a277309e89031c59d053a2ede8907ac352ff6
Cássio Marcião <marciaocassio@gmail.com>
2023-09-23 08:53:16 -0300

    chore: Update testing library react version

--- src/components/Button/tests.spec.tsx
+++ src/components/Button/tests.spec.tsx
@@ -6,13 +6,11 @@ import { Button } from '.';
 describe('Button component', () => {
     it('should render correctly', () => {
         render(<Button>Test</Button>);
-
         expect(screen.getByText('Test')).toBeInTheDocument();
     });
 
     it('should render loading in button', () => {
         render(<Button isLoading>Test</Button>);
-
         expect(screen.queryByText('Test')).not.toBeInTheDocument();
     });
 });
